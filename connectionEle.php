b9f5fa8c8bf134339d09e5751a898a904f03a00d
AchiniKatawala <30442207+AchiniKatawala@users.noreply.github.com>
2018-05-28 08:49:39 +0530

    Add basic connection filles

---
+++ connectionEle.php
@@ -0,0 +1,8 @@
+<?php
+$db_name = "elefriend";
+$mysql_username = "elefriend";
+$mysql_password = "eleFriend";
+$server_name = "elefriend.cy0qgnavpryd.ap-south-1.rds.amazonaws.com";
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+
+?>
---
+++ mainActivity.php
@@ -0,0 +1,13 @@
+<?php
+require "connectionEle.php";
+$phone_number = $_POST["phone_number"];
+$mysql_query = "select * from users where mobile_number like '$phone_number';";
+$result = mysqli_query($conn ,$mysql_query);
+
+if(mysqli_num_rows($result)>0){
+	echo "success";
+}
+else{
+	echo "not success";
+}
+?>
