3d27416c418ca0f0b70b55a656882fd4ee1310f2
AchiniKatawala <30442207+AchiniKatawala@users.noreply.github.com>
2018-05-28 10:17:49 +0530

    Change to show result

--- mainActivity.php
+++ mainActivity.php
@@ -3,7 +3,7 @@ require "connectionEle.php";
 $phone_number = $_POST["phone_number"];
 $mysql_query = "select * from users where mobile_number like '$phone_number';";
 $result = mysqli_query($conn ,$mysql_query);
-
+echo $result;
 if(mysqli_num_rows($result)>0){
 	echo "success";
 }
